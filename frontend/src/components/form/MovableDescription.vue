3bc09a2d636f18a3d7fe0b19d8403244a4dbe7a5
dodosophia <376697534@qq.com>
2020-09-28 15:21:30 +0800

    add id and sender to Extra Message

--- frontend/src/components/form/MovableDescription.vue
+++ frontend/src/components/form/MovableDescription.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <p style="line-height:16px;vertical-align:middle;">
-      <Tooltip content="Upward this section" placement="top" :delay="500">
-        <a v-if="index">
-          <Icon type="md-arrow-round-up" style="padding-right:5px;" @click="sortDesc(index)"/>
-        </a>
-      </Tooltip>
-      <strong style="padding-right:5px;">{{data.channel}}</strong>
-      <Tooltip content="Delete this section" placement="top" :delay="500">
-        <a>
-          <Icon type="md-trash" @click="deleteDesc(index)"/>
-        </a>
-      </Tooltip>
-    </p>
-    <Input v-model="data.message" type="textarea" :autosize="{ minRows: 2, maxRows: 20 }"/>
+    <Row>
+      <Col span="6">
+        <Tooltip content="Upward this section" placement="top" :delay="500">
+          <a v-if="index">
+            <Icon type="md-arrow-round-up" style="padding-right:5px;" @click="sortDesc(index)" />
+          </a>
+        </Tooltip>
+        <strong style="padding-right:5px;">{{data.channel}}</strong>
+        <Tooltip content="Delete this section" placement="top" :delay="500">
+          <a>
+            <Icon type="md-trash" @click="deleteDesc(index)" />
+          </a>
+        </Tooltip>
+      </Col>
+      <Col span="18" style=" text-align:right">
+        <Tag color="green" style="max-width:100%">{{data.sender.file}}</Tag>
+      </Col>
+    </Row>
+    <Input v-model="data.message" type="textarea" :autosize="{ minRows: 2, maxRows: 20 }" />
   </div>
 </template>
 
 <script>
 export default {
-  props: ['data', 'index'],
+  props: ["data", "index"],
   methods: {
     deleteDesc(index) {
-      this.$parent.$parent.deleteDesc(index)
+      this.$parent.$parent.deleteDesc(index);
     },
     sortDesc(index) {
-      this.$parent.$parent.sortDesc(index)
+      this.$parent.$parent.sortDesc(index);
     }
   }
-}
+};
 </script>
