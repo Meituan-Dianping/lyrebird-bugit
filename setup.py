d697b0d43d6c1a45f67a2a041fa4559762621f5e
yumiguan <yumiguan1009@gmail.com>
2022-12-02 15:34:46 +0800

    update version code

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lyrebird-bugit',
-    version='1.12.0',
+    version='1.12.1',
     packages=['lyrebird_bugit'],
     url='https://github.com/Meituan-Dianping/lyrebird-bugit',
     author='HBQA',
@@ -28,5 +28,5 @@ setup(
         ]
     },
     install_requires=[
-        "lyrebird"
+        "lyrebird>=2.10.3"
     ])
